975b102ee645e90398e16dd84f5b12612d5063e6
manjunathsirur1994 <manjunathsirur1994@gmail.com>
2025-03-14 13:13:29 +0530

    removed footer

--- Chatbot.py
+++ Chatbot.py
@@ -16,7 +16,7 @@ hide_streamlit_style = """
         <style>
         #MainMenu {visibility: hidden;}
         header {visibility: hidden;}
-        footer {visibility: hidden;}
+
         </style>
         """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
